50442c5be1a9cdd7c799205ad22b25008399fe56
robinwettstaedt <robinwettstadt@gmail.com>
2021-08-24 21:15:27 +0200

    Second Chapter done

--- mflix-js/src/dao/commentsDAO.js
+++ mflix-js/src/dao/commentsDAO.js
@@ -45,12 +45,20 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: Create/Update Comments
       // Construct the comment document to be inserted into MongoDB.
-      const commentDoc = { someField: "someValue" }
+      const commentDoc = {
+        name: user.name,
+        email: user.email,
+        movie_id: ObjectId(movieId),
+        text: comment,
+        date: date,
+      }
 
       return await comments.insertOne(commentDoc)
     } catch (e) {
       console.error(`Unable to post comment: ${e}`)
-      return { error: e }
+      return {
+        error: e,
+      }
     }
   }
 
@@ -69,15 +77,26 @@ export default class CommentsDAO {
       // TODO Ticket: Create/Update Comments
       // Use the commentId and userEmail to select the proper comment, then
       // update the "text" and "date" fields of the selected comment.
+      // here's how the update is performed
       const updateResponse = await comments.updateOne(
-        { someField: "someValue" },
-        { $set: { someOtherField: "someOtherValue" } },
+        {
+          _id: ObjectId(commentId),
+          email: userEmail,
+        },
+        {
+          $set: {
+            text: text,
+            date: date,
+          },
+        },
       )
 
       return updateResponse
     } catch (e) {
       console.error(`Unable to update comment: ${e}`)
-      return { error: e }
+      return {
+        error: e,
+      }
     }
   }
 
@@ -96,6 +115,7 @@ export default class CommentsDAO {
       // Use the userEmail and commentId to delete the proper comment.
       const deleteResponse = await comments.deleteOne({
         _id: ObjectId(commentId),
+        email: userEmail,
       })
 
       return deleteResponse
@@ -116,14 +136,31 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: User Report
       // Return the 20 users who have commented the most on MFlix.
-      const pipeline = []
+      const pipeline = [
+        {
+          $group: {
+            _id: "$email",
+            count: {
+              $sum: 1,
+            },
+          },
+        },
+        {
+          $sort: {
+            count: -1,
+          },
+        },
+        {
+          $limit: 20,
+        },
+      ]
 
       // TODO Ticket: User Report
       // Use a more durable Read Concern here to make sure this data is not stale.
-      const readConcern = comments.readConcern
+      // const readConcern = comments.readConcern
 
       const aggregateResult = await comments.aggregate(pipeline, {
-        readConcern,
+        readConcern: { level: "majority" },
       })
 
       return await aggregateResult.toArray()
